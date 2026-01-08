5655d19675cfef7eacd89188f6dc4b6c60f3fb84
Minje Jeon <i@minje.kr>
2026-01-08 15:19:05 +0900

    Bump version to v0.3.0

--- version/version.go
+++ version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version defines the current version of the xll-gen tool.
 // This version string is used in the CLI output and generated code headers.
-const Version = "v0.2.1"
+const Version = "v0.3.0"
